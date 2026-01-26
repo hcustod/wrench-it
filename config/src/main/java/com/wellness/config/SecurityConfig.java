492ee8b21e70c894cc8bd9147ecabb6b8efef8f8
hcustod <h.custodio.dev@gmail.com>
2026-01-25 19:08:47 -0500

    Start: Basic project layout

---
+++ config/src/main/java/com/wellness/config/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.wellness;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+public class SecurityConfig {
+    // We'll add more rules here later
+}
